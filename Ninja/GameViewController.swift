4ea349ee7b66d78f8909fae339093ac0e6e30388
Marcos Texeira <marcostexeira@Marcoss-MacBook-Pro.local>
2016-04-27 19:24:49 -0400

    firsttttt

---
+++ Ninja/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  Ninja
+//
+//  Created by Marcos Texeira on 4/27/16.
+//  Copyright (c) 2016 Marcos Texeira. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
